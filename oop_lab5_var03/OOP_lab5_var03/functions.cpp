78b74644368473a9621afe6d8147cb9a6bb9ed48
Max Donchenko <wereya2@gmail.com>
2016-01-21 01:31:47 +0200

    Correct version of 5th lab: <exceptions>.

--- oop_lab5_var03/OOP_lab5_var03/functions.cpp
+++ oop_lab5_var03/OOP_lab5_var03/functions.cpp
@@ -1,32 +1,44 @@
-#define _CRT_SECURE_NO_WARNINGS // for the correct using of fopen()-function
-
 #include <iostream>
 #include "functions.h"
+#include <string>
+#include <vector>
 
 using namespace std;
 
 void ReadFromFile(char *path) {
-	FILE *stream;
+	FILE *stream = NULL;
 	char s[100];
+	string str(path);
+	// black_extensions: ext-s which are not able to read
+	vector <string> black_extensions = { "exe" , "ilk", "pdb" };
 
 	try {
-		stream = fopen(path, "rb");
-		throw stream;
-	}
-	catch (FILE *stream) {
-		if (stream == NULL) {
+		string ext = str.substr(str.find_last_of(".") + 1);
+		for (auto i : black_extensions) {
+			if (i == ext) {
+				throw path;
+			}
+		}
+		try {
+			fopen_s(&stream, path, "rb");
+			if (stream == NULL) {
+				throw stream;
+			}
+			while (!feof(stream)) {
+				fgets(s, 100, stream);
+				if (!feof(stream)) {
+					printf("%s", s);
+				}
+			}
+			printf("%s\n", s); // last row of the file does not read in loop above
+
+			fclose(stream);
+		}
+		catch (FILE *stream) {
 			cout << "File not found." << endl;
-			return;
 		}
 	}
-
-	while (!feof(stream)) {
-		fgets(s, 100, stream);
-		if (!feof(stream)) {
-			printf("%s", s);
-		}
+	catch (char *path) {
+		cout << "File couldn't been read." << endl;
 	}
-	printf("%s\n", s); // last row of the file does not read in loop above
-
-	fclose(stream);
 }
--- oop_lab5_var03/OOP_lab5_var03/main.cpp
+++ oop_lab5_var03/OOP_lab5_var03/main.cpp
@@ -4,11 +4,39 @@
 using namespace std;
 
 int main() {
-	char path[20];
+	int choice;
+	char path1[50];
+	char path2[50] = "..\Debug\OOP_lab5_var03.exe";
+	char path3[50] = "OOP_lab5_var03.vcxproj.filters";
+	char path4[50] = "main.cpp";
 
-	cout << "Path to file: ";
-	cin >> path;
-	ReadFromFile(path);
+	cout << "Open file #";
+	cin >> choice;
+	cout << endl;
+
+	switch (choice)
+	{
+	case 1:
+		cout << "Path to file: ";
+		cin >> path1;
+		ReadFromFile(path1);
+		break;
+	case 2:
+		system("cls");
+		ReadFromFile(path2);
+		break;
+	case 3:
+		system("cls");
+		ReadFromFile(path3);
+		break;
+	case 4:
+		system("cls");
+		ReadFromFile(path4);
+		break;
+	default:
+		cout << "Enter 1..4" << endl;
+		break;
+	}
 
 	system("pause");
 	return 0;
